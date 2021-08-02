474c98d223157e71f241b9e069f592d0dd92498e
Peskov Sergey <sergey.production.dev@gmail.com>
2021-08-02 22:55:14 +0600

    Done - 1

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-print("Условие 1 задачи")
+print ("Найдите сумму всех чисел меньше 1000, кратных 3 или 5.")
+
+sum = 0
+
+# number = 1
+# while number < 1000:
+#     if number % 3 == 0 or number % 5 == 0:
+#         sum += number    
+#     number += 1
+
+for number in range(1000):
+    if number % 3 == 0 or number % 5 == 0:
+        sum += number
+
+print ("Сумма равна:", sum)
