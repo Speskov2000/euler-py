ab68afaf012da6a750e19eb2616586c5ed639c8a
Peskov Sergey <serpesgeikov@gmail.com>
2021-08-03 11:49:41 +0000

    Done - 2

--- 2.py
+++ 2.py
@@ -1,20 +1,17 @@
+# Каждый следующий элемент ряда Фибоначчи получается при сложении двух предыдущих. Начиная с 1 и 2, первые 10 элементов будут:
+# 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ... 
+
 print ("Найдите сумму всех четных элементов ряда Фибоначчи, которые не превышают четыре миллиона.")
-
-# step = 1
-# for number in range(0, 10, step):
-#     step = number
-#     sum += number
-#     print (number)
-
+sum = 0
 
 number = 1
 previosNumber = number
 
-while number < 100:
-    print (number)
+while number < 4000000:
+    if number % 2 == 0:
+        sum += number
     temp = number
     number += previosNumber    
     previosNumber = temp
     
-
 print (sum)
