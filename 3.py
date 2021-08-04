c9deb0d4c9e4ed69fe2338d3f757801d955d348d
Peskov Sergey <serpesgeikov@gmail.com>
2021-08-04 17:59:41 +0000

    in process - 2

--- 3.py
+++ 3.py
@@ -1,38 +1,37 @@
 # Простые делители числа 13195 - это 5, 7, 13 и 29.
 # Каков самый большой делитель числа 600851475143, являющийся простым числом?
+#################################################################################################################
+# Решение подсмотрел.
+# import math
+# def issimple(a):
+#     r=math.ceil(math.sqrt(a))
+#     lst=[]
+#     for i in range(3,r):
+#         if a%i==0:
+#             if issimple(i)==[]:lst.append(i)
+#     return lst
+# r=issimple(600851475143)
+# print(r)
 
+
+#################################################################################################################
+# Это работает, но не для больших чисел. Мой код.
 import math
-def issimple(a):
-    r=math.ceil(math.sqrt(a))
-    lst=[]
-    for i in range(3,r):
-        if a%i==0:
-            if issimple(i)==[]:
-                lst.append(i)
-    return lst
 
-dividers=issimple(26)
-max = dividers
-for divider in dividers:
-    if divider < max:
-        max = divider
+def isPrimeFactor(num):
+    numSqrt = math.ceil(math.sqrt(num))
+    for divider in range(2, numSqrt):
+        if num % divider == 0:
+            return False
+    return True 
 
-print (max)
+mainNumber = 600851475143
+number = math.ceil(math.sqrt(mainNumber))
+largestPrimeFactor = 1
 
+for divider in range(2, number):
+    if mainNumber % divider == 0 and isPrimeFactor(divider):
+        largestPrimeFactor = divider
+        print (divider)
 
-
-
-
-# Это работает, но не для больших чисел.
-# def isPrimeFactor(number):    
-#     for divider in range(2, number):
-#         if number%divider==0:return False
-#     return True 
-
-# largestPrimeFactor = 1
-# for i in range(1, 600851475144):
-#     if 600851475143 % i == 0:
-#         if isPrimeFactor(i):
-#             largestPrimeFactor = i
-
-# print ("Наибольший делитель числа 600851475143 равен:", largestPrimeFactor)
+print ("Наибольший делитель числа 600851475143 равен:", largestPrimeFactor)
