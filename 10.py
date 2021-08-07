3d87f4cc7c5992f5d34338d7a9e1a2fa62cb7746
Peskov Sergey <sergey.production.dev@gmail.com>
2021-08-07 20:33:44 +0600

    Done - 10, is waiting of pull request

--- 10.py
+++ 10.py
@@ -1,3 +1,21 @@
 # Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
 #
 # Найдите сумму всех простых чисел меньше двух миллионов.
+import math
+
+dividers = [2]
+number = 0
+
+for number in range(3, 2000000, 2):
+    status = True
+    sqrtNumber = math.ceil(number**0.5)
+    for divider in dividers:        
+        if divider > sqrtNumber:
+            break
+        if number % divider == 0:
+            status = False
+            break
+    if status:
+        dividers.append(number)
+
+print ("Сумма всех простых чисел меньше двух миллионов =", sum(dividers))
